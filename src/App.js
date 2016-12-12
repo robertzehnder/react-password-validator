ebf1fadd4f246f1d065c652f3cca4a0e4ca66bf8
Robert Zehnder <robzcodes@gmail.com>
2016-12-12 10:48:45 -0500

    Adds password validator

--- src/App.js
+++ src/App.js
@@ -3,17 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      pwd: '',
+      confirmPwd: ''
+    };
+
+    this.setEmail = this.setEmail.bind(this);
+    this.setPwd = this.setPwd.bind(this);
+    this.setConfirmPwd = this.setConfirmPwd.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  setEmail(event) {
+    this.setState({email: event.target.value});
+    event.preventDefault()
+  }
+
+  setPwd(event) {
+    this.setState({pwd: event.target.value});
+    event.preventDefault()
+  }
+
+  setConfirmPwd(event) {
+    this.setState({confirmPwd: event.target.value});
+    event.preventDefault()
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.email);
+    console.log(this.state.pwd);
+    console.log(this.state.confirmPwd);
+
+    if (this.state.pwd === this.state.confirmPwd) {
+      alert('Password Matches for: ' + this.state.email);
+      event.preventDefault();
+    }
+    else {
+      alert("Whoops...")
+    }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form onSubmit={this.handleSubmit} className="App">
+        <p><input placeholder="Email" type="text" value={this.state.email} onChange={this.setEmail}/></p>
+        <p><input placeholder="Password" type="Password" value={this.state.pwd} onChange={this.setPwd}/></p>
+        <p><input placeholder="Confirm Password" type="Password" value={this.state.confirmPwd} onChange={this.setConfirmPwd}/></p>
+        <p><input type="submit" value="Submit"/></p>
+      </form>
     );
   }
 }
